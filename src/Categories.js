b023711360687c9a883abc07318d9282988db830
madinadinaa <dinadinaa043@gmail.com>
2026-05-03 13:01:30 +0800

    ml

--- src/Categories.js
+++ src/Categories.js
@@ -1,50 +1,207 @@
 import { Component } from "react";
-class Categories extends Component  {
-    render() {
-        return (
-             <div className="container mt-5">
-                <div className="text-start">
-                      <button className="btn btn-success">Tambah Data</button>
-                </div>
-              
-                
-           <table className="table">
+import axios from "axios";
+import "./App.css";
+
+class Categories extends Component {
+ state = {
+  data: [],
+  name: "",
+  icon: "",
+  type: "",
+  editId: null,
+  showForm: false, // 🔥 ini penting
+  errors: {},
+};
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  // 🔥 READ
+  getData = async () => {
+    const token = localStorage.getItem("token");
+
+    const res = await axios.get(
+      "http://127.0.0.1:8000/api/categories",
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    this.setState({ data: res.data.data.categories });
+  };
+
+  // 🔥 HANDLE INPUT
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  // 🔥 CREATE & UPDATE
+ handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const token = localStorage.getItem("token");
+  const { name, icon, type, editId } = this.state;
+
+  try {
+    if (editId) {
+      await axios.put(
+        `http://127.0.0.1:8000/api/categories/${editId}`,
+        { name, icon, type },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+    } else {
+      await axios.post(
+        "http://127.0.0.1:8000/api/categories",
+        { name, icon, type },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+    }
+
+    // ✅ reset setelah sukses
+    this.setState({
+      name: "",
+      icon: "",
+      type: "",
+      editId: null,
+      showForm: false,
+      errors: {}, // reset error
+    });
+
+    this.getData();
+
+  } catch (err) {
+    console.log(err.response.data); // 🔥 lihat error backend
+
+    this.setState({
+      errors: err.response?.data?.errors || {},
+    });
+  }
+};
+
+  // 🔥 EDIT (ISI FORM)
+  handleEdit = (item) => {
+    this.setState({
+      name: item.name,
+      icon: item.icon,
+      type: item.type,
+      editId: item.id,
+    });
+  };
+
+  render() {
+    return (
+      <div className="content">
+        <button
+  className="add-btn"
+  onClick={() => this.setState({ showForm: true, editId: null })}
+>
+  Tambah Data
+</button>
+     {this.state.showForm && (
+  <form onSubmit={this.handleSubmit}>
+    <input
+      name="name"
+      placeholder="Nama"
+      value={this.state.name}
+      onChange={this.handleChange}
+      className="input"
+    />
+
+    {this.state.errors.name && (
+  <p className="error">{this.state.errors.name[0]}</p>
+)}
+
+    <input
+      name="icon"
+      placeholder="Icon"
+      value={this.state.icon}
+      onChange={this.handleChange}
+      className="input"
+    />
+
+    {this.state.errors.icon && (
+  <p className="error">{this.state.errors.icon[0]}</p>
+)}
+
+    <input
+      name="type"
+      placeholder="Type"
+      value={this.state.type}
+      onChange={this.handleChange}
+      className="input"
+    />
+{this.state.errors.type && (
+  <p className="error">{this.state.errors.type[0]}</p>
+)}
+    <button className="add-btn">
+      {this.state.editId ? "Update" : "Simpan"}
+    </button>
+
+    {/* tombol batal */}
+    <button
+      type="button"
+      className="btn"
+      onClick={() =>
+        this.setState({
+          showForm: false,
+          name: "",
+          icon: "",
+          type: "",
+          editId: null,
+        })
+      }
+    >
+      Batal
+    </button>
+  </form>
+)}
+
+        {/* TABLE */}
+        <div className="table-container">
+          <table>
             <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>Icon</th>
-                    <th>Type</th>
-                    <th>Aksi</th>
-                </tr>
+              <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Icon</th>
+                <th>Type</th>
+                <th>Aksi</th>
+              </tr>
             </thead>
+
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Gaji Bulanan</td>
-                    <td>📓</td>
-                    <td>INCOME</td>
-                    <td>
-                        <button className="btn btn-warning m-2">Edit</button>
-                        <button className="btn btn-danger m-2">Hapus</button>
-                    </td>
+              {this.state.data.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{index + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.icon}</td>
+                  <td>{item.type}</td>
+                  <td>
+                    <button
+                      className="action-btn edit"
+                      onClick={() => this.handleEdit(item)}
+                    >
+                      Edit
+                    </button>
+
+                    <button
+                      className="action-btn delete"
+                      onClick={() => this.handleDelete(item.id)}
+                    >
+                      Hapus
+                    </button>
+                  </td>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Makanan</td>
-                    <td>🍔</td>
-                    <td>EXPENSE</td>
-                    <td>
-                        <button className="btn btn-warning m-2">Edit</button>
-                        <button className="btn btn-danger m-2">Hapus</button>
-                    </td>
-                </tr>
+              ))}
             </tbody>
 
-           </table>
-            </div>
-        )
-    }
+          </table>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default Categories;
--- src/Currencies.js
+++ src/Currencies.js
@@ -1,50 +1,194 @@
 import { Component } from "react";
-class Currencies extends Component  {
-    render() {
-        return (
-             <div className="container mt-5">
-                <div className="text-start">
-                      <button className="btn btn-success">Tambah Data</button>
-                </div>
-              
-                
-           <table className="table">
+import axios from "axios";
+import "./App.css";
+
+class Currencies extends Component {
+ state = {
+  data: [],
+  name: "",
+  symbol: "",
+  code: "",
+  editId: null,
+  showForm: false, // 🔥 ini penting
+};
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  // 🔥 READ
+  getData = async () => {
+    const token = localStorage.getItem("token");
+
+    const res = await axios.get(
+      "http://127.0.0.1:8000/api/currencies",
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    this.setState({ data: res.data.data.currencies });
+  };
+
+  // 🔥 HANDLE INPUT
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  // 🔥 CREATE & UPDATE
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+    const { name, symbol, code, editId } = this.state;
+
+    if (editId) {
+      // UPDATE
+      await axios.put(
+        `http://127.0.0.1:8000/api/currencies/${editId}`,
+        { name, symbol, code },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+    } else {
+      // CREATE
+      await axios.post(
+        "http://127.0.0.1:8000/api/currencies",
+        { name, symbol, code },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+    }
+
+    this.setState({ name: "", symbol: "", code: "", editId: null });
+    this.getData();
+  };
+
+  // 🔥 DELETE
+  handleDelete = async (id) => {
+    const token = localStorage.getItem("token");
+
+    await axios.delete(
+      `http://127.0.0.1:8000/api/currencies/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    this.getData();
+  };
+
+  // 🔥 EDIT (ISI FORM)
+  handleEdit = (item) => {
+    this.setState({
+      name: item.name,
+      symbol: item.symbol,
+      code: item.code,
+      editId: item.id,
+    });
+  };
+
+  render() {
+    return (
+      <div className="content">
+        <button
+  className="add-btn"
+  onClick={() => this.setState({ showForm: true, editId: null })}
+>
+  Tambah Data
+</button>
+     {this.state.showForm && (
+  <form onSubmit={this.handleSubmit}>
+    <input
+      name="name"
+      placeholder="Nama"
+      value={this.state.name}
+      onChange={this.handleChange}
+      className="input"
+    />
+
+    <input
+      name="symbol"
+      placeholder="Symbol"
+      value={this.state.symbol}
+      onChange={this.handleChange}
+      className="input"
+    />
+
+    <input
+      name="code"
+      placeholder="Code"
+      value={this.state.code}
+      onChange={this.handleChange}
+      className="input"
+    />
+
+    <button className="add-btn">
+      {this.state.editId ? "Update" : "Simpan"}
+    </button>
+
+    {/* tombol batal */}
+    <button
+      type="button"
+      className="btn"
+      onClick={() =>
+        this.setState({
+          showForm: false,
+          name: "",
+          symbol: "",
+          code: "",
+          editId: null,
+        })
+      }
+    >
+      Batal
+    </button>
+  </form>
+)}
+
+        {/* TABLE */}
+        <div className="table-container">
+          <table>
             <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>Symbol</th>
-                    <th>Code</th>
-                    <th>Aksi</th>
-                </tr>
+              <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Symbol</th>
+                <th>Code</th>
+                <th>Aksi</th>
+              </tr>
             </thead>
+
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Rupiah</td>
-                    <td>Rp</td>
-                    <td>IDR</td>
-                    <td>
-                        <button className="btn btn-warning m-2">Edit</button>
-                        <button className="btn btn-danger m-2">Hapus</button>
-                    </td>
+              {this.state.data.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{index + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.symbol}</td>
+                  <td>{item.code}</td>
+                  <td>
+                    <button
+                      className="action-btn edit"
+                      onClick={() => this.handleEdit(item)}
+                    >
+                      Edit
+                    </button>
+
+                    <button
+                      className="action-btn delete"
+                      onClick={() => this.handleDelete(item.id)}
+                    >
+                      Hapus
+                    </button>
+                  </td>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Dolar</td>
-                    <td>$</td>
-                    <td>USD</td>
-                    <td>
-                        <button className="btn btn-warning m-2">Edit</button>
-                        <button className="btn btn-danger m-2">Hapus</button>
-                    </td>
-                </tr>
+              ))}
             </tbody>
 
-           </table>
-            </div>
-        )
-    }
+          </table>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default Currencies;
